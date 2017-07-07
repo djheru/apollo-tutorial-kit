713b898999d3d2ddc34ca544783836004a1600ca
Philip Damra <philip.damra@gstv.com>
2017-07-07 15:10:16 -0400

    Using sequelize and sqlite for persistence with updated resolver

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,21 +1,19 @@
+import {Author} from './connectors';
+
 const resolvers = {
 	Query: {
 		author(root, args) {
-			const {firstName, lastName} = args;
-			return {id: 1, firstName, lastName};
+			return Author.find({where: args})
 		}
 	},
 	Author: {
 		posts(author) {
-			return [
-				{ id: 1, title: 'A post', text: 'Some text', views: 2},
-				{ id: 2, title: 'Another post', text: 'Some other text', views: 200}
-			]
+			return author.getPosts();
 		}
 	},
 	Post: {
 		author(post) {
-			return { id: 1, firstName: 'Hello', lastName: 'World' };
+			post.getAuthor();
 		}
 	}
 };
