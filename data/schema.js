b6203620955314926c7ca37c8619af119a42ece2
Philip Damra <philip.damra@gstv.com>
2017-07-07 09:38:12 -0400

    Add bloggy schema

--- data/schema.js
+++ data/schema.js
@@ -5,8 +5,22 @@ import {
 import mocks from './mocks'
 
 const typeDefs = `
+type Author {
+	id: Int
+	firstName: String
+	lastName: String
+	posts: [Post]
+}
+type Post {
+	id: Int
+	title: String
+	text: String
+	views: Int
+	author: Author
+}
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  getFortuneCookie: String
 }
 `;
 
